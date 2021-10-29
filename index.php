5e3be09fb663788112b389bab474232527378f08
Miguel Padrron <mikyeduards@gmail.com>
2021-10-29 11:31:29 -0400

    listo con constructor y con destructor dentro de una class Personas

--- index.php
+++ index.php
@@ -3,17 +3,27 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 
-include_once "funciones/Personas.php";
-// esto deberia vista
-echo "<br>";
-$resp = LoginUsuario('mp','1234567');
+include_once "Clases/Personas.php";
 
-if( $resp['si'] ){
+
+
+
+$objecto = new Personas();
+echo $objecto->imprimirHola('Miguel Padron');
+
+
+
+
+// esto deberia vista
+// echo "<br>";
+// $resp = LoginUsuario('mp','1234567');
+
+// if( $resp['si'] ){
     
-    echo imprimirHola("Miguel Padron",50000). " y su rol dentro del sistema es ".$resp['nombre-rol'];
-}else{
+//     echo imprimirHola("Miguel Padron",50000). " y su rol dentro del sistema es ".$resp['nombre-rol'];
+// }else{
     
-    echo "Usted no esta en el sistema contactar al admin";
-}
+//     echo "Usted no esta en el sistema contactar al admin";
+// }
 
 ?>
