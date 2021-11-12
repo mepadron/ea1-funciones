46841939d64620a34e3df1fc7aad6d2501210401
Miguel Padrron <mikyeduards@gmail.com>
2021-11-12 11:24:38 -0400

    se creovalidacion desde cliente y una libreria en poo con php para obtener la baseUrl del proyecto

--- Clases/Login.php
+++ Clases/Login.php
@@ -2,9 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
+include_once "Libreria/helpers.php";
 class LoginUsuario{
 
-
+    
     public function LoginUsuario($loginF, $claveF){
 
         $loginBD = "mp";
@@ -22,15 +23,41 @@ class LoginUsuario{
     
     public function obtenerDatosFormularios($l,$c){
         // var_dump($l);    
-        // var_dump($c);    
-        if($l=="mep" and $c=="1234"){
-            // echo "usted esta logueado dentro del sistema";
-            return true;
-        }else{
-            // echo "NO ESTA logueado dentro del sistema";
-            return false;
+        // var_dump($c);
+        // print_r(!empty($l))."<br>";
+        // print_r(!empty($c))."<br>";
+        //  if(!empty($l) or !empty($c)){
+            
+            if(empty($l)){
+                echo "Login esta vacio<br>";
+
+            }
+            if(empty($c)){
+                echo "Clave esta vacio<br>";
+
+            }else{
+                //si el formato de correo es valido
+
+                if($l=="mep" and $c=="1234"){
+                    return true;
+                }else{
+                    // echo "NO ESTA logueado dentro del sistema";
+                    return false;
     
-        }
+                }
+            }
+
+        //  }else{
+        //      echo "las caja de textos estan vacia<br>";
+        //  }      
+        // if($l=="mep" and $c=="1234"){
+        //     // echo "usted esta logueado dentro del sistema";
+        //     return true;
+        // }else{
+        //     // echo "NO ESTA logueado dentro del sistema";
+        //     return false;
+    
+        // }
     }
     
 
